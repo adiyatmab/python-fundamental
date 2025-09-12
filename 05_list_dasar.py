270290e13cda9e7d2ff08f9a100fe4f59b06ef2f
adiyatmab <buyungo415@gmail.com>
2025-09-12 17:11:34 +0700

    Selesai: Latihan dasar membuat, mengubah, dan menambah item pada list

---
+++ 05_list_dasar.py
@@ -0,0 +1,10 @@
+tamu_undangan = ["Khresna","Asa","Natya"]
+print("Tamu undangan awal:", tamu_undangan)
+
+# Tamu pertama tidak bisa datang, ganti dengan tamu baru
+tamu_undangan[0] = "Rizky"
+print("Tamu undangan setelah diganti :", tamu_undangan)
+
+# Menambahkan tamu baru di akhir list
+tamu_undangan.append("Nino")
+print("Tamu undangan setelah ditambah :", tamu_undangan)
