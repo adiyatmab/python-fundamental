6a3df3d91990fb394e788707ffe8f723ec5fc3f8
adiyatmab <buyungo415@gmail.com>
2025-09-12 16:51:23 +0700

    Selesai: Latihan penggabungan string dan konversi tipe data

---
+++ 03_string_manipulation.py
@@ -0,0 +1,6 @@
+nama = "Buyung"
+kota_asal = "Semarang"
+umur = 24
+# Menggunakan snake_case untuk nama variabel
+perkenalan = "Halo, nama saya " + nama + ", saya berasal dari " + kota_asal + " dan umur saya " + str(umur) + " tahun."
+print(perkenalan)
