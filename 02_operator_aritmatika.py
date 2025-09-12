042490b8eeed048e3806e5578750d8546bae3dd3
adiyatmab <buyungo415@gmail.com>
2025-09-12 16:50:11 +0700

    Selesai : Latihan operator aritmatika

---
+++ 02_operator_aritmatika.py
@@ -0,0 +1,17 @@
+# Latihan 2: Operator Aritmatika
+# Tujuan: Menggunakan operator matematika dasar (*) untuk melakukan perhitungan.
+
+# --- Kode Anda Dimulai Di Sini ---
+
+# Membuat variabel untuk data belanja
+harga_buku = 15000
+jumlah_buku = 3
+
+# Melakukan proses kalkulasi
+total_harga = harga_buku * jumlah_buku
+
+# Menampilkan hasil akhir dalam format struk sederhana
+print("--- Struk Belanja Sederhana ---")
+print("Harga satuan buku: Rp", harga_buku)
+print("Jumlah buku yang dibeli:", jumlah_buku)
+print("Total yang harus dibayar adalah: Rp", total_harga)
