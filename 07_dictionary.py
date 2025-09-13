1e90ea412714df53daa495b00fe5373b07c27494
Adiyatma Buyung <buyungo415@gmail.com>
2025-09-13 07:27:18 +0700

    Selesai: Latihan membuat, mengakses, dan mengubah dictionary

---
+++ 07_dictionary.py
@@ -0,0 +1,10 @@
+spek_laptop = {
+    "brand": "Dell",
+    "cpu": "Intel Core i7",
+    "ram": 16
+}
+
+print("CPU Laptop:", spek_laptop["cpu"])
+spek_laptop["ram"] = 32
+spek_laptop["storage"] = "1TB SSD"
+print("Spesifikasi Laptop:", spek_laptop)
