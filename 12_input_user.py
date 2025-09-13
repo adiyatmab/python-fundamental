cd09954ad7cafef6c663e68efeb4e3225fdcaf61
Adiyatma Buyung <buyungo415@gmail.com>
2025-09-13 08:18:32 +0700

    Selesai: Latihan input dari user dan konversi tipe data

---
+++ 12_input_user.py
@@ -0,0 +1,15 @@
+angka_pertama = int(input("\nMasukkan angka pertama: "))
+angka_kedua = int(input("Masukkan angka kedua: "))
+
+jumlah = angka_pertama + angka_kedua
+print(f"Hasil penjumlahan {angka_pertama} + {angka_kedua} = {jumlah}")
+
+# Atau
+angka_pertama = input("\nMasukkan angka pertama: ")
+angka_kedua = input("Masukkan angka kedua: ")
+
+angka_pertama_int = int(angka_pertama)
+angka_kedua_int = int(angka_kedua)
+
+jumlah = angka_pertama_int + angka_kedua_int
+print(f"Hasil penjumlahan {angka_pertama_int} + {angka_kedua_int} = {jumlah}")
